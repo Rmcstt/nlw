f0b84fa75a8ef27400e2792f9b149f1d150f08d9
rmcstt <anisio.nanato@hotmail.com>
2022-01-21 10:15:19 -0300

    adjusted 2

--- main.js
+++ main.js
@@ -14,3 +14,16 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 }
+/*mudar pagina quando der scroll*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    //scroll émaior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    //menor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
